46a6729ea68b6ef0787dc135b54b439e046cac4c
jamil <git@github.com:jhuseynzada/git.git>
2023-12-04 09:57:19 +0400

    data display and filtering

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const productList = document.getElementById("productList");
+const productSearch = document.getElementById("productSearch");
+const filterByCategory = document.getElementById("filterByCategory");
 
 function createProductCard(product) {
     const card = document.createElement("div");
@@ -30,13 +32,53 @@ function createProductCard(product) {
     return card;
 }
 
-function displayProducts(products) {
+var productsArray = []
+
+function renderProducts(products) {
+    productList.innerHTML = '';
     products.forEach(product => {
         productList.appendChild(createProductCard(product));
     });
+    console.log(productsArray)
 }
 
-fetch("https://dummyjson.com/products")
+// Function to apply filters and show the corresponding products
+const applyProductFilters = () => {
+    const keyword = productSearch.value.toLowerCase();
+    const chosenCategory = filterByCategory.value;
+
+    const productsToShow = productsArray.filter(product => {
+        const keywordMatch = product.title.toLowerCase().includes(keyword) ||
+            product.brand.toLowerCase().includes(keyword) ||
+            product.description.toLowerCase().includes(keyword) ||
+            (product.category && product.category.toLowerCase().includes(keyword));
+        const categoryMatch = !chosenCategory || product.category === chosenCategory;
+        return keywordMatch && categoryMatch;
+    });
+    renderProducts(productsToShow);
+};
+
+// Event listeners for search and category filter
+productSearch.addEventListener('input', applyProductFilters);
+filterByCategory.addEventListener('change', applyProductFilters);
+
+fetch("https://dummyjson.com/products?limit=60")
     .then(response => response.json())
-    .then(data => displayProducts(data.products))
+    .then(data => {
+        productsArray = data.products
+        renderProducts(productsArray)
+    })
     .catch(error => console.error("Failed to fetch products:", error));
+
+fetch("https://dummyjson.com/products/categories")
+    .then(res => res.json())
+    .then(categories => {
+        console.log(categories)
+        categories.forEach(category => {
+            const option = document.createElement('option');
+            option.value = category;
+            option.textContent = category;
+            filterByCategory.appendChild(option);
+        });
+    })
+    .catch(err => console.log(err));
